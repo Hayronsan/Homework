1da6f7f746ac83efda91237ff0f49eabdc916f76
Hayronsan <hayronsan@gmail.com>
2024-04-12 18:36:40 +0500

    Initial Commit

---
+++ Homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework
+//
+//  Created by Nodir on 12/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
